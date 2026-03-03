101a4780fbb49b9c65a77e8e30d8b8a9d0ef43be
notlim_dev <elmotigre6@gmail.com>
2026-03-03 13:58:01 -0500

    Fish: add interactive alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,14 +1,52 @@
+#.·:'''''''''''''''''''''''''''''''''''''''''''''''''''':·.
+#: : ┌┬┐┌─┐┌┬┐┌─┐┬┬  ┌─┐┌─┐  ┌┐┌┌─┐┌┬┐┬  ┬┌┬┐┌┬┐┌─┐┬  ┬ : :
+#: :  │││ │ │ ├┤ ││  ├┤ └─┐  ││││ │ │ │  ││││ ││├┤ └┐┌┘ : :
+#: : ─┴┘└─┘ ┴ └  ┴┴─┘└─┘└─┘  ┘└┘└─┘ ┴ ┴─┘┴┴ ┴─┴┘└─┘ └┘  : :
+#: :                  [FISH]                            : : 
+#: :                                                    : : 
+#: :  [🚀 GITHUB]:https://github.com/notlimdev          : : 
+#'·:....................................................:·'
+
 if status is-interactive
-    # Función para Neovim con configuración personalizada
-    function nvim_lazyvim
-        env NVIM_APPNAME=nvim_lazyvim nvim $argv
+
+    # Lista archivos con iconos y color.
+    function ls
+        eza --icons always --color always $argv
     end
 
-    function nvim_nvchad
-        env NVIM_APPNAME=nvim_nvchad nvim $argv
+    # Lista con detalles en formato largo.
+    function ll
+        eza --icons always --color always -la $argv
     end
 
-    function nvim_astrovim
-        env NVIM_APPNAME=nvim_astrovim nvim $argv
+    # Muestra arbol excluyendo carpetas comunes.
+    function lt
+        eza -T --icons --color always --group-directories-first \
+            --ignore-glob 'node_modules|.git|dist|build' $argv
+    end
+
+    # Muestra arbol con profundidad 2.
+    function lt2
+        eza -T -L 2 --icons --color always --group-directories-first $argv
+    end
+
+    # Lista en largo ordenando por tipo.
+    function la
+        eza -la --icons --color always --group-directories-first --sort=type $argv
+    end
+
+    # Lista solo directorios.
+    function ld
+        eza -a --only-dirs --icons --color always $argv
+    end
+
+    # Lista solo archivos (sin dirs).
+    function lf
+        eza -a --only-files --icons --color always $argv
+    end
+
+    # Lista en largo ordenando por fecha.
+    function ltnew
+        eza -la --icons --color always --sort=modified $argv
     end
 end
